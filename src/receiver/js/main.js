aa0c8c3804d287ba5efabaa8f2ce7bb3ea5395af
gongzhen <517441540@qq.com>
2025-06-03 19:58:57 +0800

    fix: bugfix

--- src/receiver/js/main.js
+++ src/receiver/js/main.js
@@ -11,176 +11,200 @@ let renderstreaming;
 /** @type {boolean} */
 let useWebSocket;
 
-const codecPreferences = document.getElementById('codecPreferences');
-const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
-  'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
-const messageDiv = document.getElementById('message');
-messageDiv.style.display = 'none';
+export const start = (renderRoot) => {
+  const codecPreferences = renderRoot.querySelector("#codecPreferences");
+  const supportsSetCodecPreferences =
+    window.RTCRtpTransceiver &&
+    "setCodecPreferences" in window.RTCRtpTransceiver.prototype;
 
-const playerDiv = document.getElementById('player');
-const lockMouseCheck = document.getElementById('lockMouseCheck');
-const videoPlayer = new VideoPlayer();
 
-setup();
 
-window.document.oncontextmenu = function () {
-  return false;     // cancel default menu
-};
+  const messageDiv = renderRoot.querySelector("#message");
 
-window.addEventListener('resize', function () {
-  videoPlayer.resizeVideo();
-}, true);
+  console.log(messageDiv);
 
-window.addEventListener('beforeunload', async () => {
-  if(!renderstreaming)
-    return;
-  await renderstreaming.stop();
-}, true);
+  messageDiv.style.display = "none";
 
-async function setup() {
-  const res = await getServerConfig();
-  useWebSocket = res.useWebSocket;
-  showWarningIfNeeded(res.startupMode);
-  showCodecSelect();
-  showPlayButton();
-}
+  const playerDiv = renderRoot.querySelector("#player");
+  const lockMouseCheck = renderRoot.querySelector("#lockMouseCheck");
+  const videoPlayer = new VideoPlayer();
 
-function showWarningIfNeeded(startupMode) {
-  const warningDiv = document.getElementById("warning");
-  if (startupMode == "private") {
-    warningDiv.innerHTML = "<h4>Warning</h4> This sample is not working on Private Mode.";
-    warningDiv.hidden = false;
+  setup();
+
+  window.document.oncontextmenu = function () {
+    return false; // cancel default menu
+  };
+
+  window.addEventListener(
+    "resize",
+    function () {
+      videoPlayer.resizeVideo();
+    },
+    true
+  );
+
+  window.addEventListener(
+    "beforeunload",
+    async () => {
+      if (!renderstreaming) return;
+      await renderstreaming.stop();
+    },
+    true
+  );
+
+  async function setup() {
+    const res = await getServerConfig();
+    useWebSocket = res.useWebSocket;
+    showWarningIfNeeded(res.startupMode);
+    showCodecSelect();
+    showPlayButton();
   }
-}
 
-function showPlayButton() {
-  if (!document.getElementById('playButton')) {
-    const elementPlayButton = document.createElement('img');
-    elementPlayButton.id = 'playButton';
-    elementPlayButton.src = '../../images/Play.png';
-    elementPlayButton.alt = 'Start Streaming';
-    playButton = document.getElementById('player').appendChild(elementPlayButton);
-    playButton.addEventListener('click', onClickPlayButton);
+  function showWarningIfNeeded(startupMode) {
+    const warningDiv = renderRoot.querySelector("#warning");
+    if (startupMode == "private") {
+      warningDiv.innerHTML =
+        "<h4>Warning</h4> This sample is not working on Private Mode.";
+      warningDiv.hidden = false;
+    }
   }
-}
 
-function onClickPlayButton() {
-  playButton.style.display = 'none';
+  function showPlayButton() {
+    if (!renderRoot.getElementById("playButton")) {
+      const elementPlayButton = document.createElement("img");
+      elementPlayButton.id = "playButton";
+      elementPlayButton.src = "../../images/Play.png";
+      elementPlayButton.alt = "Start Streaming";
+      playButton = renderRoot
+        .getElementById("player")
+        .appendChild(elementPlayButton);
+      playButton.addEventListener("click", onClickPlayButton);
+    }
+  }
 
-  // add video player
-  videoPlayer.createPlayer(playerDiv, lockMouseCheck);
-  setupRenderStreaming();
-}
+  function onClickPlayButton() {
+    playButton.style.display = "none";
 
-async function setupRenderStreaming() {
-  codecPreferences.disabled = true;
+    // add video player
+    videoPlayer.createPlayer(playerDiv, lockMouseCheck);
+    setupRenderStreaming();
+  }
 
-  const signaling = useWebSocket ? new WebSocketSignaling() : new Signaling();
-  const config = getRTCConfiguration();
-  renderstreaming = new RenderStreaming(signaling, config);
-  renderstreaming.onConnect = onConnect;
-  renderstreaming.onDisconnect = onDisconnect;
-  renderstreaming.onTrackEvent = (data) => videoPlayer.addTrack(data.track);
-  renderstreaming.onGotOffer = setCodecPreferences;
+  async function setupRenderStreaming() {
+    codecPreferences.disabled = true;
 
-  await renderstreaming.start();
-  await renderstreaming.createConnection();
-}
+    const signaling = useWebSocket ? new WebSocketSignaling() : new Signaling();
+    const config = getRTCConfiguration();
+    renderstreaming = new RenderStreaming(signaling, config);
+    renderstreaming.onConnect = onConnect;
+    renderstreaming.onDisconnect = onDisconnect;
+    renderstreaming.onTrackEvent = (data) => videoPlayer.addTrack(data.track);
+    renderstreaming.onGotOffer = setCodecPreferences;
 
-function onConnect() {
-  const channel = renderstreaming.createDataChannel("input");
-  videoPlayer.setupInput(channel);
-  showStatsMessage();
-}
+    await renderstreaming.start();
+    await renderstreaming.createConnection();
+  }
 
-async function onDisconnect(connectionId) {
-  clearStatsMessage();
-  messageDiv.style.display = 'block';
-  messageDiv.innerText = `Disconnect peer on ${connectionId}.`;
+  function onConnect() {
+    const channel = renderstreaming.createDataChannel("input");
+    videoPlayer.setupInput(channel);
+    showStatsMessage();
+  }
 
-  await renderstreaming.stop();
-  renderstreaming = null;
-  videoPlayer.deletePlayer();
-  if (supportsSetCodecPreferences) {
+  async function onDisconnect(connectionId) {
+    clearStatsMessage();
+    messageDiv.style.display = "block";
+    messageDiv.innerText = `Disconnect peer on ${connectionId}.`;
+
+    await renderstreaming.stop();
+    renderstreaming = null;
+    videoPlayer.deletePlayer();
+    if (supportsSetCodecPreferences) {
+      codecPreferences.disabled = false;
+    }
+    showPlayButton();
+  }
+
+  function setCodecPreferences() {
+    /** @type {RTCRtpCodecCapability[] | null} */
+    let selectedCodecs = null;
+    if (supportsSetCodecPreferences) {
+      const preferredCodec =
+        codecPreferences.options[codecPreferences.selectedIndex];
+      if (preferredCodec.value !== "") {
+        const [mimeType, sdpFmtpLine] = preferredCodec.value.split(" ");
+        const { codecs } = RTCRtpSender.getCapabilities("video");
+        const selectedCodecIndex = codecs.findIndex(
+          (c) => c.mimeType === mimeType && c.sdpFmtpLine === sdpFmtpLine
+        );
+        const selectCodec = codecs[selectedCodecIndex];
+        selectedCodecs = [selectCodec];
+      }
+    }
+
+    if (selectedCodecs == null) {
+      return;
+    }
+    const transceivers = renderstreaming
+      .getTransceivers()
+      .filter((t) => t.receiver.track.kind == "video");
+    if (transceivers && transceivers.length > 0) {
+      transceivers.forEach((t) => t.setCodecPreferences(selectedCodecs));
+    }
+  }
+
+  function showCodecSelect() {
+    if (!supportsSetCodecPreferences) {
+      messageDiv.style.display = "block";
+      messageDiv.innerHTML = `Current Browser does not support <a href="https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpTransceiver/setCodecPreferences">RTCRtpTransceiver.setCodecPreferences</a>.`;
+      return;
+    }
+
+    const codecs = RTCRtpSender.getCapabilities("video").codecs;
+    codecs.forEach((codec) => {
+      if (["video/red", "video/ulpfec", "video/rtx"].includes(codec.mimeType)) {
+        return;
+      }
+      const option = document.createElement("option");
+      option.value = (codec.mimeType + " " + (codec.sdpFmtpLine || "")).trim();
+      option.innerText = option.value;
+      codecPreferences.appendChild(option);
+    });
     codecPreferences.disabled = false;
   }
-  showPlayButton();
-}
 
-function setCodecPreferences() {
-  /** @type {RTCRtpCodecCapability[] | null} */
-  let selectedCodecs = null;
-  if (supportsSetCodecPreferences) {
-    const preferredCodec = codecPreferences.options[codecPreferences.selectedIndex];
-    if (preferredCodec.value !== '') {
-      const [mimeType, sdpFmtpLine] = preferredCodec.value.split(' ');
-      const { codecs } = RTCRtpSender.getCapabilities('video');
-      const selectedCodecIndex = codecs.findIndex(c => c.mimeType === mimeType && c.sdpFmtpLine === sdpFmtpLine);
-      const selectCodec = codecs[selectedCodecIndex];
-      selectedCodecs = [selectCodec];
-    }
+  /** @type {RTCStatsReport} */
+  let lastStats;
+  /** @type {number} */
+  let intervalId;
+
+  function showStatsMessage() {
+    intervalId = setInterval(async () => {
+      if (renderstreaming == null) {
+        return;
+      }
+
+      const stats = await renderstreaming.getStats();
+      if (stats == null) {
+        return;
+      }
+
+      const array = createDisplayStringArray(stats, lastStats);
+      if (array.length) {
+        messageDiv.style.display = "block";
+        messageDiv.innerHTML = array.join("<br>");
+      }
+      lastStats = stats;
+    }, 1000);
   }
 
-  if (selectedCodecs == null) {
-    return;
-  }
-  const transceivers = renderstreaming.getTransceivers().filter(t => t.receiver.track.kind == "video");
-  if (transceivers && transceivers.length > 0) {
-    transceivers.forEach(t => t.setCodecPreferences(selectedCodecs));
-  }
-}
-
-function showCodecSelect() {
-  if (!supportsSetCodecPreferences) {
-    messageDiv.style.display = 'block';
-    messageDiv.innerHTML = `Current Browser does not support <a href="https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpTransceiver/setCodecPreferences">RTCRtpTransceiver.setCodecPreferences</a>.`;
-    return;
-  }
-
-  const codecs = RTCRtpSender.getCapabilities('video').codecs;
-  codecs.forEach(codec => {
-    if (['video/red', 'video/ulpfec', 'video/rtx'].includes(codec.mimeType)) {
-      return;
+  function clearStatsMessage() {
+    if (intervalId) {
+      clearInterval(intervalId);
     }
-    const option = document.createElement('option');
-    option.value = (codec.mimeType + ' ' + (codec.sdpFmtpLine || '')).trim();
-    option.innerText = option.value;
-    codecPreferences.appendChild(option);
-  });
-  codecPreferences.disabled = false;
-}
-
-/** @type {RTCStatsReport} */
-let lastStats;
-/** @type {number} */
-let intervalId;
-
-function showStatsMessage() {
-  intervalId = setInterval(async () => {
-    if (renderstreaming == null) {
-      return;
-    }
-
-    const stats = await renderstreaming.getStats();
-    if (stats == null) {
-      return;
-    }
-
-    const array = createDisplayStringArray(stats, lastStats);
-    if (array.length) {
-      messageDiv.style.display = 'block';
-      messageDiv.innerHTML = array.join('<br>');
-    }
-    lastStats = stats;
-  }, 1000);
-}
-
-function clearStatsMessage() {
-  if (intervalId) {
-    clearInterval(intervalId);
+    lastStats = null;
+    intervalId = null;
+    messageDiv.style.display = "none";
+    messageDiv.innerHTML = "";
   }
-  lastStats = null;
-  intervalId = null;
-  messageDiv.style.display = 'none';
-  messageDiv.innerHTML = '';
-}
+};
--- src/streaming-video.ts
+++ src/streaming-video.ts
@@ -1,46 +1,97 @@
 import { html, css, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+// @ts-ignore
+import { start } from "./receiver/js/main";
+
 @customElement("streaming-video")
 export class StreamingVideo extends LitElement {
   static styles = css`
-    p {
-      color: blue;
+    #player {
+      position: relative;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      align-items: center;
+      justify-content: center;
+      display: flex;
+      background-color: #323232;
+    }
+
+    #player:before {
+      content: "";
+      display: block;
+      padding-top: 66%;
+    }
+
+    #playButton {
+      width: 15%;
+      max-width: 200px;
+      cursor: pointer;
+    }
+
+    #Video {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+
+    #fullscreenButton {
+      position: absolute;
+      top: 25px;
+      right: 25px;
+      width: 32px;
+      height: 32px;
     }
   `;
 
   @property()
   name!: string;
 
- 
-  private _progressInterval: number | undefined;
-
   firstUpdated() {
-    let progress = 0;
-    this._progressInterval = window.setInterval(() => {
-      progress += 10;
-      // 创建并触发 progress 事件
-      const event = new Event('progress', { bubbles: true });
-      this.dispatchEvent(event);
-      
-      if (progress >= 100) {
-        clearInterval(this._progressInterval);
-      }
-    }, 500);
+    start(this.renderRoot);
   }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    if (this._progressInterval) {
-      clearInterval(this._progressInterval);
-    }
-  }
-  
 
   render() {
     return html`
-      <div>
-        <video id="video" width="640" height="360" controls></video>
+      <div id="container">
+        <div id="warning" hidden="true"></div>
+        <div id="player"></div>
+
+        <div class="box">
+          <span>Codec preferences:</span>
+          <select id="codecPreferences" autocomplete="off" disabled>
+            <option selected value="">Default</option>
+          </select>
+        </div>
+
+        <div class="box">
+          <span>Lock Cursor to Player:</span>
+          <input type="checkbox" id="lockMouseCheck" autocomplete="off" />
+        </div>
+
+        <p>
+          For more information about sample, see
+          <a
+            href="https://docs.unity3d.com/Packages/com.unity.renderstreaming@3.1/manual/sample-broadcast.html"
+            >Broadcast sample</a
+          >
+          document page.
+        </p>
+
+        <div id="message"></div>
+
+        <section>
+          <a
+            href="https://github.com/Unity-Technologies/UnityRenderStreaming/tree/develop/WebApp/client/public/receiver"
+            title="View source for this page on GitHub"
+            id="viewSource"
+            >View source on GitHub</a
+          >
+        </section>
       </div>
     `;
   }
